bc9a0bb7006b35e81a7dfdad583e8b432405391a
Daniel <daniel.paul@hope.edu>
2023-05-01 23:41:34 -0400

    Modified addDiceToSheet to add the current roll to an array. Current role would be set to 0 after that. By inputing the same number that dice would be removed from the array.

--- Player.java
+++ Player.java
@@ -1,5 +1,6 @@
 //import Dice.java;
 //import Sheet.java;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Player {
     //Player needs to have access to the dice and sheet.
@@ -8,11 +9,14 @@ public class Player {
     Dice playerDice;
     int currentRollResult1, currentRollResult2, currentRollResult3,
         currentRollResult4, currentRollResult5 = 0;
-    Sheet playerSheet;
+    //Change this into an array with a size of 5 and make sure to add the numbers to the specific spots corresponding to the dice.
+    //Array setAsideDice;
+    //Sheet playerSheet;
 
     public Player(){
         playerDice = new Dice();
-        playerSheet = new Sheet();
+        //setAsideDice = new Array<Integer>();
+        //playerSheet = new Sheet();
     }
 
      public void playerRollDice(){
@@ -28,11 +32,12 @@ public class Player {
          System.out.println(currentRollResult5);
      }
 
-    public void addDiceToSheet(){
+    public void addDiceToSheet(Sheet playerSheet){
         Scanner choice = new Scanner(System.in);
         boolean finished = false;
         System.out.println("Please choose a dice to add to your sheet.");
         System.out.println("Press any number from 1-5 to choose a die or 0 if you don't want to pick any.");
+        System.out.println("If you want to revert a choice you made, type the same number to remove the dice from the sheet.");
         int dieNumberChoice = choice.nextInt();
         while(finished == false){
             if(dieNumberChoice == 0){
@@ -40,24 +45,45 @@ public class Player {
                 break;
             }
             else if(dieNumberChoice == 1){
-                playerSheet.addToSheet(currentRollResult1);
-                System.out.println("You have added " + currentRollResult1 + " to your sheet.");
+                if(currentRollResult1 == 0){
+                    playerSheet.removeFromSheet(currentRollResult1);
+
+                    
+                }
+                else{
+                    playerSheet.addToSheet(currentRollResult1);
+                    //setAsideDice.add(currentRollResult1);
+                    currentRollResult1 = 0;
+                    System.out.println("You have added " + currentRollResult1 + " to your sheet.");
+                }
+
             }
             else if(dieNumberChoice == 2){
                 playerSheet.addToSheet(currentRollResult2);
-                System.out.println();
+                //setAsideDice.add(currentRollResult2);
+                currentRollResult2 = 0;
+                System.out.println("You have added " + currentRollResult2 + " to your sheet.");
             }
             else if(dieNumberChoice == 3){
                 playerSheet.addToSheet(currentRollResult3);
-                System.out.println();
+                //setAsideDice.add(currentRollResult3);
+                currentRollResult3 = 0;
+                System.out.println("You have added " + currentRollResult3 + " to your sheet.");
             }
             else if(dieNumberChoice == 4){
                 playerSheet.addToSheet(currentRollResult4);
-                System.out.println();
+                //setAsideDice.add(currentRollResult4);
+                currentRollResult4 = 0;
+                System.out.println("You have added " + currentRollResult4 + " to your sheet.");
             }   
             else if(dieNumberChoice == 5){
+                if(currentRollResult5 == 0){
+                    System.out.println("You");
+                }
                 playerSheet.addToSheet(currentRollResult5);
-                System.out.println();
+                //setAsideDice.add(currentRollResult5);
+                currentRollResult5 = 0;
+                System.out.println("You have added " + currentRollResult5 + " to your sheet.");
             }
             else{
                 System.out.println("Please pick a valid number from 0-5!");
